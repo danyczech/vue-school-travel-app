cf7b519c616e2631052296f5b31a8edb8e335b9c
danakal <dana.kalcikova@centrum.cz>
2023-02-26 13:39:44 +0100

    route props - boolean mode

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ const routes = [
     path: "/destination/:id/:slug",
     name: "destination.show",
     component: () => import("@/views/DestinationShow.vue"),
+    props: true,
   },
 ];
 
--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="destination">
     <h1>{{ destination.name }}</h1>
-    <div class="destination-details">
+      <div class="destination-details">
       <img :src="`/images/${destination.image}`" :alt="destination.name" />
       <p>{{ destination.description }}</p>
     </div>
@@ -12,14 +12,13 @@
 import sourceData from "@/data.json";
 
 export default {
+  props:{
+    id: {type: String, required: true},
+  },
   computed: {
-    destinationId() {
-      return parseInt(this.$route.params.id);
-    },
-
     destination() {
       return sourceData.destinations.find(
-        (destination) => destination.id === this.destinationId
+        (destination) => destination.id === parseInt(this.id)
       );
     },
   },
